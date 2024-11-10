d66eda16bc015a53c67c8279e881e23b6126459c
VexuBGM <emoniksp@gmail.com>
2024-11-10 15:56:04 +0200

    Custom url length and non-repeatable long urls

--- server.js
+++ server.js
@@ -22,18 +22,27 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/shortURLs', async (req, res) => {
+  const existingFullURL = await ShortURL.findOne({ full: req.body.fullURL });
+  if (existingFullURL) {
+    return res.status(400).send('A short URL for this long URL already exists');
+  }
+
   let shortURL;
   if (req.body.customShortURL) {
     shortURL = req.body.customShortURL;
     if (shortURL.length < 5 || shortURL.length > 10) {
       return res.status(400).send('Custom short URL must be between 5 and 10 characters');
     }
-    const existingURL = await ShortURL.findOne({ short: shortURL });
-    if (existingURL) {
+    const existingShortURL = await ShortURL.findOne({ short: shortURL });
+    if (existingShortURL) {
       return res.status(400).send('Custom short URL already exists');
     }
   } else {
-    shortURL = shortId.generate();
+    const urlLength = parseInt(req.body.urlLength, 10);
+    if (isNaN(urlLength) || urlLength < 5 || urlLength > 10) {
+      return res.status(400).send('URL length must be between 5 and 10 characters');
+    }
+    shortURL = shortId.generate().slice(0, urlLength);
   }
 
   await ShortURL.create({ full: req.body.fullURL, short: shortURL });
@@ -41,7 +50,7 @@ app.post('/shortURLs', async (req, res) => {
 });
 
 app.get('/:shortURL', async (req, res) => {
-  const shortURL = await ShortURL.findOne({ short: req.params.shortURL })
+  const shortURL = await ShortURL.findOne({ short: req.params.shortURL });
   if (shortURL == null) return res.sendStatus(404);
 
   shortURL.clicks++;
